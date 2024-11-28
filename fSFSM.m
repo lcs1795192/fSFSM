36cdd1f078d577a5076c506194492f5904bda8ad
lcs1795192 <1845251476@qq.com>
2024-11-28 11:28:44 +0800

    rename symbols to match the manuscript, 重命名符号以匹配文章内容

--- fSFSM.m
+++ fSFSM.m
@@ -1,4 +1,4 @@
-function Qfo=fSFSM(Y, Phi, r)
+function Qfo=fSFSM(Y, X, r)
 %% fSFSM 矩阵最小二乘法
 % Ref: Woodbury K A, Najafi H, Monte F D, et al. Inverse Heat Conduction:
 % Ill-Posed Problems[M]. 2nd. Hoboken:John Wiley & Sons, 2023.
@@ -8,16 +8,16 @@ function Qfo=fSFSM(Y, Phi, r)
 % [Ns, Nt+1] =size(Y)
 % [Ns, Nf, Nt]=size(Phi);
 % r is is the number of time steps including future data, default is 2.
-[Ns, Nf, Nt]=size(Phi);
+[Ns, Nf, Nt]=size(X);
 Qfo=zeros(Nf, Nt-(r-1));
 deltaY=Y(:, 2:end)-Y(:, 1);
-dPhi=diff(Phi, 1, 3);
-X=[];
+dPhi=diff(X, 1, 3);
+Xp=[];
 for ir=1:r
-    X=cat(1, X, Phi(:, :, ir));
+    Xp=cat(1, Xp, X(:, :, ir));
 end
-coefficientMatrix=X'*X;
-k=coefficientMatrix^-1*X';
+coefficientMatrix=Xp'*Xp;
+k=coefficientMatrix^-1*Xp';
 for iNt=1:Nt-(r-1)
     That=zeros(Ns, r);
     deltaT=[];
